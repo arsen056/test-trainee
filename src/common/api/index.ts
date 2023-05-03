913ef6a4179516cf3f003ecf5ef7fe0892417ea3
arsen056 <vurdihanov@gmail.com>
2023-05-03 13:09:53 +0300

    fix: cors error

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import {INewsState} from "common/api/types";
 
+const CORS_URL = 'https://cors-anywhere.herokuapp.com/'
+
 const instance = axios.create({
-  baseURL: 'https://newsapi.org/v2/',
+  baseURL: `${CORS_URL}https://newsapi.org/v2/`,
   headers: {
     Authorization: '7b95c4fcb9694e23b9c225736e359217'
   }
